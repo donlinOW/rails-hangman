6e84c9601a75d7d91f2ac45b0cfbbd77ced89196
Don Lin <don.lin@optimalworkshop.com>
2025-01-17 10:35:23 +1300

    Refactor games_controller to use local vars instead of instance vars to hold 'games' and 'game'

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -3,43 +3,43 @@ class GamesController < ApplicationController
 
   # list all games
   def index
-    @games = Game.all
-    render json: @games
+    games = Game.all
+    render json: games
   end
 
   # create a new game
   def create
-    @game = Game.create
-    render json: @game
+    game = Game.create
+    render json: game
   end
 
   # show a specific game
   def show
-    @game = Game.find(params[:id])
+    game = Game.find(params[:id])
     render json: {
-      word: @game.display,
-      lives: @game.lives,
-      guessed_letters: @game.guessed_letters.split(''),
-      state: @game.state
+      word: game.display,
+      lives: game.lives,
+      guessed_letters: game.guessed_letters.split(''),
+      state: game.state
     }
   end
 
   # guess a letter
   def update
-    @game = Game.find(params[:id])
-    @game.process_guess(params[:letter]) if params[:letter]
+    game = Game.find(params[:id])
+    game.process_guess(params[:letter]) if params[:letter]
     render json: {
-      word: @game.display,
-      lives: @game.lives,
-      guessed_letters: @game.guessed_letters.split(''),
-      state: @game.state
+      word: game.display,
+      lives: game.lives,
+      guessed_letters: game.guessed_letters.split(''),
+      state: game.state
     }
   end
 
   # delete a game
   def destroy
-    @game = Game.find(params[:id])
-    @game.destroy
+    game = Game.find(params[:id])
+    game.destroy
     head :no_content
   end
 end
