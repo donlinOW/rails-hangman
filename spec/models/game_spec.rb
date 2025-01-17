2a37d69be4cdaedb1c90c407790421a44ef7bbad
Don Lin <don.lin@optimalworkshop.com>
2025-01-17 13:02:39 +1300

    Add unit test for game model's random_word method

--- spec/models/game_spec.rb
+++ spec/models/game_spec.rb
@@ -9,4 +9,13 @@ RSpec.describe Game, type: :model do
       expect(game.display).to eq('_' * game.word.length)
     end
   end
+
+  describe '#random_word' do
+    it 'returns a random word from the words.txt file' do
+      game = Game.new
+      word = game.random_word
+      words = File.readlines(Rails.root.join('lib', 'words.txt')).map(&:chomp)
+      expect(words).to include(word)
+    end
+  end
 end
